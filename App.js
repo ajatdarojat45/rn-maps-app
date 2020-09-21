65e361510bfb08289827bd25d0228927d1b3c631
Ajat Darojat <ajatdarojat45@gmail.com>
2020-09-21 16:06:05 +0700

    init

--- App.js
+++ App.js
@@ -32,17 +32,23 @@ export default function App() {
   const [routes, setRoutes] = useState([]);
   useEffect(() => {
     fetch(
-      `https://maps.googleapis.com/maps/api/directions/json?origin=${markers[0].coordinate.latitude}, ${markers[0].coordinate.longitude}&key=YOURAPIKEY=${markers[1].coordinate.latitude}, ${markers[1].coordinate.longitude}&mode=walking`
+      `https://maps.googleapis.com/maps/api/directions/json?origin=${markers[0].coordinate.latitude}, ${markers[0].coordinate.longitude}&key=YOURAPIKEY&destination=${markers[1].coordinate.latitude}, ${markers[1].coordinate.longitude}&mode=walking`
     )
       .then((resp) => resp.json())
       .then((data) => {
         var tempRoutes = data.routes[0].legs[0].steps.map((step, i) => {
           return {
-            latitude: step.start_location.lat,
-            longitude: step.start_location.lng,
+            latitude: step.end_location.lat,
+            longitude: step.end_location.lng,
           };
         });
-        setRoutes(tempRoutes);
+        setRoutes([
+          {
+            latitude: markers[0].coordinate.latitude,
+            longitude: markers[0].coordinate.longitude,
+          },
+          ...tempRoutes,
+        ]);
       });
   }, [markers]);
 
